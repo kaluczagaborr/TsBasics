2faab658d69b12d90ee6681a55ad58383471c162
Kalucza Gabor <anahle@citromail.hu>
2020-08-12 19:24:32 +0200

    sec comm

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, ShowResult) {
+    if (ShowResult == true)
+        console.log(n1 + n2);
+    else
+        return n1 + n2;
 }
 var number1 = 23.7;
 var number2 = 6;
-var res = add(number1, number2);
-console.log(res);
+var printRes = true;
+add(number1, number2, printRes);
--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
-function add(n1: number, n2: number){
-return n1+n2;
+function add(n1: number, n2: number, ShowResult: boolean){
+
+    if(ShowResult == true) console.log(n1 + n2)
+    else return n1+n2;
+
+    
+
 }
 
 const number1 = 23.7;
 const number2 = 6;
 
-const res = add(number1,number2);
+const printRes = true;
+add(number1,number2,printRes);
 
-console.log(res);
